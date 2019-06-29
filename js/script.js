c2cdf2e2ed345cbc6981d4d59a49a40965822f4d
IlyaOrluk <misfits120@mail.ru>
2019-06-29 03:20:36 +0300

    add preview slider iPhone

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 let canvas,
     c,
     getCanvasSize = () => {
@@ -44,7 +43,6 @@ let increment = wave.frequency,
 
 }
 setInterval(animate, 1000/60)
-// window.addEventListener('resize', getCanvasSize);
 
 
 
@@ -53,26 +51,53 @@ setInterval(animate, 1000/60)
 let slides = document.querySelectorAll('.slider-iphone-item'),
     moveSlideLeft = document.querySelector('.slider-iphone-left'),
     moveSlideRight = document.querySelector('.slider-iphone-right'),
+    slidesInfo = document.querySelectorAll('.iphone-info'),
+    previewItem = document.querySelectorAll('.iphones-preview-item'),
     slidesPosition = 0,
-    activeSlide = 0
-    
-    moveSlideRight.addEventListener('click', () => {
-        slideWidth = Math.floor(slides[activeSlide].getBoundingClientRect().width)
-        console.log(slideWidth+50,activeSlide)
-        slidesPosition += slideWidth+50;
-        activeSlide += 1; 
-        slides.forEach((item) => {
-            item.style.cssText = `transform: translateX(-${slidesPosition}px)`;
+    activeSlide = 0,
+    fadeSlideInfo = () => {
+        slidesInfo.forEach((item)=>{
+            item.style.display = 'none';
         });
+        slidesInfo[activeSlide].style.display = 'block';
+    }
+    fadeSlideInfo();
+    moveSlideRight.addEventListener('click', () => {
+        slideWidth = Math.floor(slides[activeSlide].getBoundingClientRect().height)
+        if(activeSlide == slides.length-1){
+            activeSlide = 0;
+            slidesPosition = 0;
+        } else {
+            activeSlide += 1;
+            slidesPosition += slideWidth;
+        } 
+        slides.forEach((item) => {
+            item.style.cssText = `transform: translateY(-${slidesPosition}px)`;
+        });
+        fadeSlideInfo();
     });
     moveSlideLeft.addEventListener('click', () => {
-        slideWidth = Math.floor(slides[activeSlide-1].getBoundingClientRect().width)
-        slidesPosition -= slideWidth+50;
-        activeSlide -= 1;
+        slideWidth = Math.floor(slides[activeSlide].getBoundingClientRect().height)
+        if(activeSlide == 0){
+            activeSlide = slides.length-1;
+            slidesPosition = slideWidth*(slides.length-1);
+        } else {
+            activeSlide -= 1;
+            slidesPosition -= slideWidth;
+        } 
         slides.forEach((item) => {
-            item.style.cssText = `transform: translateX(${slidesPosition}px)`;
+            item.style.cssText = `transform: translateY(-${slidesPosition}px)`;
         });
-        console.log(slideWidth+50,activeSlide)
+        fadeSlideInfo();
     });
-    console.log(slides[0].getBoundingClientRect().width+50);
-
+    previewItem.forEach((item, i) => {
+        item.addEventListener('click', () =>{
+            slideWidth = Math.floor(slides[i].getBoundingClientRect().height)
+            activeSlide = i;
+            slidesPosition = slideWidth*i;
+            slides.forEach((item) => {
+                item.style.cssText = `transform: translateY(-${slidesPosition}px)`;
+            });
+            fadeSlideInfo();
+        });
+    });
